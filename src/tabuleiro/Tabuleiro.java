2f752b5f3a2ce03a848f4a0436503b6a91410cd6
Leandro Guina <guina86@gmail.com>
2019-03-19 20:56:55 -0300

    Adicionando pecas no tabuleiro

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -5,30 +5,40 @@ public class Tabuleiro {
 	private int fileiras;
 	private int colunas;
 	private Peca[][] pecas;
+
 	public Tabuleiro(int fileiras, int colunas) {
 		this.fileiras = fileiras;
 		this.colunas = colunas;
 		pecas = new Peca[fileiras][colunas];
 	}
-	public int getFileiras () {
+
+	public int getFileiras() {
 		return fileiras;
 	}
+
 	public void setFileiras(int linhas) {
 		this.fileiras = linhas;
 	}
+
 	public int getColunas() {
 		return colunas;
 	}
+
 	public void setColunas(int colunas) {
 		this.colunas = colunas;
 	}
-	
-	public Peca peca (int linha, int coluna) {
+
+	public Peca peca(int linha, int coluna) {
 		return pecas[linha][coluna];
 	}
-	
-	public Peca peca (Posicao posicao) {
+
+	public Peca peca(Posicao posicao) {
 		return pecas[posicao.getFileira()][posicao.getColuna()];
 	}
-	
+
+	public void colocaPeca(Peca peca, Posicao posicao) {
+		pecas[posicao.getFileira()][posicao.getColuna()] = peca;
+		peca.posicao = posicao;
+	}
+
 }
