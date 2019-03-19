f09d1f7fdeecf059df4b564adbd32da47787998d
Leandro Guina <guina86@gmail.com>
2019-03-19 19:41:07 -0300

    Adicionado class Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,11 +1,15 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ola Mundo!");
-
+		Posicao pos = new Posicao(3, 5);
+		
+		System.out.println(pos);
+		
 	}
 
 }
