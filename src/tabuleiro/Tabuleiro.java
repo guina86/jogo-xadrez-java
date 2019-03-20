5e9b2d555f90539bb16e32190059da5a6da4b8d2
Leandro Guina <guina86@gmail.com>
2019-03-19 22:16:47 -0300

    Adicionado ExcecaoDeTabuleiro e programacao defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int fileiras, int colunas) {
+		if(fileiras < 1 || colunas < 1) {
+			throw new ExcecaoDeTabuleiro("Erro ao criar o tabuleiro: Tem que ter pelo menos 1 fileira e 1 coluna");
+		}
 		this.fileiras = fileiras;
 		this.colunas = colunas;
 		pecas = new Peca[fileiras][colunas];
@@ -16,29 +19,45 @@ public class Tabuleiro {
 		return fileiras;
 	}
 
-	public void setFileiras(int linhas) {
-		this.fileiras = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
-	public Peca peca(int linha, int coluna) {
-		return pecas[linha][coluna];
+	public Peca peca(int fileira, int coluna) {
+		if(!existePosicao(fileira, coluna)) {
+			throw new ExcecaoDeTabuleiro("Posicao nao existe no tabuleiro");
+		}
+		return pecas[fileira][coluna];
 	}
 
 	public Peca peca(Posicao posicao) {
+		if(!existePosicao(posicao)) {
+			throw new ExcecaoDeTabuleiro("Posicao nao existe no tabuleiro");
+		}
 		return pecas[posicao.getFileira()][posicao.getColuna()];
 	}
 
 	public void colocaPeca(Peca peca, Posicao posicao) {
+		if(temPeca(posicao)) {
+			throw new ExcecaoDeTabuleiro("Ja existe peca nesta posicao");
+		}
 		pecas[posicao.getFileira()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean existePosicao (int fileira, int coluna) {
+		return fileira >= 0 && fileira < fileiras && coluna >= 0 && coluna < colunas; 
+	}
+	
+	public boolean existePosicao(Posicao posicao) {
+		return existePosicao(posicao.getFileira(), posicao.getColuna());
+	}
+	
+	public boolean temPeca(Posicao posicao) {
+		if(!existePosicao(posicao)) {
+			throw new ExcecaoDeTabuleiro("Posicao nao existe no tabuleiro");
+		}
+		return peca(posicao) != null;
 	}
 
 }
