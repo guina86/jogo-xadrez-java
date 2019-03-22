da839fe564c8bd1054e283382a858b9e9429a99a
Leandro Guina <guina86@gmail.com>
2019-03-21 22:37:14 -0300

    Adicionado movimento especial Ao passar

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -21,6 +21,7 @@ public class PartidaDeXadrez {
 	private Tabuleiro tabuleiro;
 	private boolean xeque;
 	private boolean xequeMate;
+	private PecaDeXadrez vulneravelAoPassar;
 
 	private List<Peca> pecasNoTabuleiro = new ArrayList<>();
 	private List<Peca> pecasCapturadas = new ArrayList<>();
@@ -46,6 +47,10 @@ public class PartidaDeXadrez {
 
 	public boolean getXequeMate() {
 		return xequeMate;
+	}
+
+	public PecaDeXadrez getVulneravelAoPassar() {
+		return vulneravelAoPassar;
 	}
 
 	public PecaDeXadrez[][] getPecas() {
@@ -76,12 +81,22 @@ public class PartidaDeXadrez {
 			throw new ExcecaoDeXadrez("Voce nao pode se colocar em Xeque");
 		}
 
+		PecaDeXadrez pecaMovida = (PecaDeXadrez)tabuleiro.peca(destino);
+
 		xeque = testaXeque(oponente(jogadorAtual));
 
 		if (testaXequeMate(oponente(jogadorAtual))) {
 			xequeMate = true;
 		} else {
 			proximoTurno();
+		}
+
+		// #movimento especial Ao passar
+		if (pecaMovida instanceof Peao && (destino.getFileira() == origem.getFileira() - 2
+				|| destino.getFileira() == origem.getFileira() + 2)) {
+			vulneravelAoPassar = pecaMovida;
+		} else {
+			vulneravelAoPassar = null;
 		}
 
 		return (PecaDeXadrez) pecaCapturada;
@@ -116,6 +131,21 @@ public class PartidaDeXadrez {
 			torre.incrementaContadorDeMovimentos();
 		}
 
+		// #movimento especial Ao passar
+		if (p instanceof Peao) {
+			if (origem.getColuna() != destino.getColuna() && pecaCapturada == null) {
+				Posicao posicaoDoPeao;
+				if (p.getCor() == Cor.BRANCO) {
+					posicaoDoPeao = new Posicao(destino.getFileira() + 1, destino.getColuna());
+				} else {
+					posicaoDoPeao = new Posicao(destino.getFileira() - 1, destino.getColuna());
+				}
+				pecaCapturada = tabuleiro.removePeca(posicaoDoPeao);
+				pecasCapturadas.add(pecaCapturada);
+				pecasNoTabuleiro.remove(pecaCapturada);
+			}
+		}
+
 		return pecaCapturada;
 	}
 
@@ -145,6 +175,20 @@ public class PartidaDeXadrez {
 			PecaDeXadrez torre = (PecaDeXadrez) tabuleiro.removePeca(destinoT);
 			tabuleiro.colocaPeca(torre, origemT);
 			torre.decrementaContadorDeMovimentos();
+		}
+
+		// #movimento especial Ao passar
+		if (p instanceof Peao) {
+			if (origem.getColuna() != destino.getColuna() && pecaCapturada == vulneravelAoPassar) {
+				PecaDeXadrez peao = (PecaDeXadrez)tabuleiro.removePeca(destino);
+				Posicao posicaoDoPeao;
+				if (p.getCor() == Cor.BRANCO) {
+					posicaoDoPeao = new Posicao(3, destino.getColuna());
+				} else {
+					posicaoDoPeao = new Posicao(4, destino.getColuna());
+				}
+				tabuleiro.colocaPeca(peao, posicaoDoPeao);
+			}
 		}
 
 	}
@@ -239,14 +283,14 @@ public class PartidaDeXadrez {
 		// colocaNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		// colocaNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
 		colocaNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('c', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('d', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('e', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('f', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('g', 2, new Peao(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('h', 2, new Peao(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('c', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('d', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('e', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('f', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('g', 2, new Peao(tabuleiro, Cor.BRANCO, this));
+		colocaNovaPeca('h', 2, new Peao(tabuleiro, Cor.BRANCO, this));
 
 		colocaNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
 		colocaNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
@@ -256,14 +300,14 @@ public class PartidaDeXadrez {
 		colocaNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocaNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
 		colocaNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('c', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('d', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('e', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('f', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('g', 7, new Peao(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('h', 7, new Peao(tabuleiro, Cor.PRETO));
+		colocaNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('c', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('d', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('e', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('f', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('g', 7, new Peao(tabuleiro, Cor.PRETO, this));
+		colocaNovaPeca('h', 7, new Peao(tabuleiro, Cor.PRETO, this));
 
 	}
 
--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -3,15 +3,18 @@ package xadrez.pecas;
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Cor;
+import xadrez.PartidaDeXadrez;
 import xadrez.PecaDeXadrez;
 
 public class Peao extends PecaDeXadrez {
 
-	public Peao(Tabuleiro tabuleiro, Cor cor) {
-		super(tabuleiro, cor);
+	private PartidaDeXadrez partidaDeXadrez;
 
+	public Peao(Tabuleiro tabuleiro, Cor cor, PartidaDeXadrez partidaDeXadrez) {
+		super(tabuleiro, cor);
+		this.partidaDeXadrez = partidaDeXadrez;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "P";
@@ -22,15 +25,16 @@ public class Peao extends PecaDeXadrez {
 		boolean[][] mat = new boolean[getTabuleiro().getFileiras()][getTabuleiro().getColunas()];
 
 		Posicao p = new Posicao(0, 0);
-		
-		if(getCor() == Cor.BRANCO) {
+
+		if (getCor() == Cor.BRANCO) {
 			p.setValores(posicao.getFileira() - 1, posicao.getColuna());
 			if (getTabuleiro().existePosicao(p) && !getTabuleiro().temPeca(p)) {
 				mat[p.getFileira()][p.getColuna()] = true;
 			}
 			p.setValores(posicao.getFileira() - 2, posicao.getColuna());
 			Posicao p2 = new Posicao(posicao.getFileira() - 1, posicao.getColuna());
-			if (getTabuleiro().existePosicao(p) && !getTabuleiro().temPeca(p) && getTabuleiro().existePosicao(p2) && !getTabuleiro().temPeca(p2) && getContadorDeMovimento() == 0) {
+			if (getTabuleiro().existePosicao(p) && !getTabuleiro().temPeca(p) && getTabuleiro().existePosicao(p2)
+					&& !getTabuleiro().temPeca(p2) && getContadorDeMovimento() == 0) {
 				mat[p.getFileira()][p.getColuna()] = true;
 			}
 			p.setValores(posicao.getFileira() - 1, posicao.getColuna() - 1);
@@ -41,6 +45,21 @@ public class Peao extends PecaDeXadrez {
 			if (getTabuleiro().existePosicao(p) && temPecaDoAdversario(p)) {
 				mat[p.getFileira()][p.getColuna()] = true;
 			}
+
+			// #movimento especial Ao passar branco
+			if (posicao.getFileira() == 3) {
+				Posicao esquerda = new Posicao(posicao.getFileira(), posicao.getColuna() - 1);
+				if (getTabuleiro().existePosicao(esquerda) && temPecaDoAdversario(esquerda)
+						&& getTabuleiro().peca(esquerda) == partidaDeXadrez.getVulneravelAoPassar()) {
+					mat[esquerda.getFileira() - 1][esquerda.getColuna()] = true;
+				}
+				Posicao direita = new Posicao(posicao.getFileira(), posicao.getColuna() + 1);
+				if (getTabuleiro().existePosicao(direita) && temPecaDoAdversario(direita)
+						&& getTabuleiro().peca(direita) == partidaDeXadrez.getVulneravelAoPassar()) {
+					mat[direita.getFileira() - 1][direita.getColuna()] = true;
+				}
+			}
+
 		} else {
 			p.setValores(posicao.getFileira() + 1, posicao.getColuna());
 			if (getTabuleiro().existePosicao(p) && !getTabuleiro().temPeca(p)) {
@@ -48,7 +67,8 @@ public class Peao extends PecaDeXadrez {
 			}
 			p.setValores(posicao.getFileira() + 2, posicao.getColuna());
 			Posicao p2 = new Posicao(posicao.getFileira() + 1, posicao.getColuna());
-			if (getTabuleiro().existePosicao(p) && !getTabuleiro().temPeca(p) && getTabuleiro().existePosicao(p2) && !getTabuleiro().temPeca(p2) && getContadorDeMovimento() == 0) {
+			if (getTabuleiro().existePosicao(p) && !getTabuleiro().temPeca(p) && getTabuleiro().existePosicao(p2)
+					&& !getTabuleiro().temPeca(p2) && getContadorDeMovimento() == 0) {
 				mat[p.getFileira()][p.getColuna()] = true;
 			}
 			p.setValores(posicao.getFileira() + 1, posicao.getColuna() - 1);
@@ -59,9 +79,23 @@ public class Peao extends PecaDeXadrez {
 			if (getTabuleiro().existePosicao(p) && temPecaDoAdversario(p)) {
 				mat[p.getFileira()][p.getColuna()] = true;
 			}
+
+			// #movimento especial Ao passar preto
+			if (posicao.getFileira() == 4) {
+				Posicao esquerda = new Posicao(posicao.getFileira(), posicao.getColuna() - 1);
+				if (getTabuleiro().existePosicao(esquerda) && temPecaDoAdversario(esquerda)
+						&& getTabuleiro().peca(esquerda) == partidaDeXadrez.getVulneravelAoPassar()) {
+					mat[esquerda.getFileira() + 1][esquerda.getColuna()] = true;
+				}
+				Posicao direita = new Posicao(posicao.getFileira(), posicao.getColuna() + 1);
+				if (getTabuleiro().existePosicao(direita) && temPecaDoAdversario(direita)
+						&& getTabuleiro().peca(direita) == partidaDeXadrez.getVulneravelAoPassar()) {
+					mat[direita.getFileira() + 1][direita.getColuna()] = true;
+				}
+			}
 		}
 
 		return mat;
 	}
-	
+
 }
